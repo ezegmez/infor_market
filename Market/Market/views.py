5f5e3a3af1cdc70cac697ab0e07ca8c9defc0fdb
ezegmez <ezegmez00@gmail.com>
2021-12-05 10:23:25 -0300

    eliminando todo

--- Market/Market/views.py
+++
@@ -1,6 +0,0 @@
-# Cuando hacemos ENTER, estamos generando una petición. Eso es lo que recibe en parametro, una request es una petición.
-from django.shortcuts import render
-
-def inicio(request):
-    # Renderizamos un templete
-    return render(request, "inicio.html")
