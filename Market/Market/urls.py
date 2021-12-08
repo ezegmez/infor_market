ba5a83c8d78a6520a985d7d89213b1d907ef98af
ezegmez <ezegmez00@gmail.com>
2021-12-08 14:16:38 -0300

    template bae

--- Market/Market/urls.py
+++ Market/Market/urls.py
@@ -1,14 +1,19 @@
-from . import views
-from django.contrib import admin
+from django.contrib      import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path
-# from os import name
+from django.urls         import path, include
+
+from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.inicio, name="inicio"),
-    path('login/', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
-    path('logout/', auth_views.logout_then_login, name="logout")
+    path('', views.inicio, name='inicio'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.logout_then_login, name='logout'),
+
+    # Includes
+    path('producto/', include('apps.productos.urls'))
 ]
+
+
 
---
+++ Market/apps/productos/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+
+app_name = 'productos'
+
+urlpatterns = [
+	path('detalle/', views.detalle, name='detalle')
+]
